250f958c4d8dc6cc02d44a97ecad1637d694b438
Kyle Robinson Young <kyle@dontkry.com>
2015-07-23 10:38:12 -0700

    Fix for elements who dont change their offset when resizing

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('mutationobserver-shim')
 module.exports = function (target, callback) {
-  var lastWidth = target.offsetWidth
-  var lastHeight = target.offsetHeight
+  function makeid (el) {
+    return [target.style.width, target.style.height, target.clientWidth, target.clientHeight].join('')
+  }
+  var last = makeid(target)
   var observer = new MutationObserver(function (mutations) {
     for (var i = 0; i < mutations.length; i++) {
       var mutation = mutations[i]
       if (mutation.attributeName !== 'style') continue
-      if (target.offsetWidth !== lastWidth || target.offsetHeight !== lastHeight) {
-        lastWidth = target.offsetWidth
-        lastHeight = target.offsetHeight
+      var now = makeid(target)
+      if (now !== last) {
+        last = now
         callback()
         break
       }
--- test.js
+++ test.js
@@ -1,10 +1,35 @@
 var onResize = require('./index.js')
 var test = require('tape')
+var objectAssign = require('object-assign')
 
 test('got resize event', function (t) {
   t.plan(1)
   var expected = parseInt(Math.random() * 1000, 10) + 'px'
   var div = document.createElement('div')
+  document.body.appendChild(div)
+  onResize(div, function () {
+    t.equal(div.style.width, expected, 'triggered on width change')
+    t.end()
+  })
+  div.style.width = expected
+})
+
+test('with center positioned element', function (t) {
+  t.plan(1)
+  var expected = parseInt(Math.random() * 1000, 10) + 'px'
+  var div = document.createElement('div')
+  objectAssign(div.style, {
+    position: 'relative',
+    width: '50em',
+    height: '20em',
+    top: '50%',
+    left: '50%',
+    padding: '3em',
+    'margin-left': '-25em',
+    'margin-top': '-10em',
+    overflow: 'hidden',
+    'transition-duration': '3s'
+  })
   document.body.appendChild(div)
   onResize(div, function () {
     t.equal(div.style.width, expected, 'triggered on width change')
